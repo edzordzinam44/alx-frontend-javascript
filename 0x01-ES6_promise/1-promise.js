dccf58a7e4bb3023c7486db072d93ce470c52ccc
edzordzinam44 <edzordzinam.alipui@gmail>
2024-06-12 04:11:17 +0000

    Modified the promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,5 @@
-export default async function getFullResponseFromAPI(success) {
-  if (success) {
-      return {
-          status: 200,
-          body: 'Success',
-      };
-  } else {
-      throw new Error('The fake API is not working currently');
-  }
+export default function getFullResponseFromAPI(success) {
+    return success
+        ? Promise.resolve({ status: 200, body: 'Success' })
+        : Promise.reject(new Error('The fake API is not working currently'));
 }
