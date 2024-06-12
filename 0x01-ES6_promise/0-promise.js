6e6719c4cd9253239ca5b8277ad9b4fe2ecf8125
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-06-12 02:47:16 +0000

    Make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,3 @@
-// Export the function as the default export of this module
 export default function getResponseFromAPI() {
   // Return a new Promise object
   return new Promise((resolve, reject) => {
