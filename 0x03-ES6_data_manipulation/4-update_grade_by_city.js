2a946dd0ee889d338f4fe87fac17fbe01e61a8de
edzordzinam44 <edzordzinam.alipui@gmail>
2024-06-16 16:01:43 +0000

    Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,29 @@
+/**
+ * Updates the grades of a list of students in a given city.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The city of students.
+ * @param {{
+ *   studentId: Number,
+ *   grade: Number,
+ * }[]} newGrades - The new grades to be given to a student.
+ * @returns {{id: Number, firstName: String, location: String, grade: String|Number}[]}
+ */
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeInfo = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeInfo ? gradeInfo.grade : 'N/A',
+      };
+    });
+}
